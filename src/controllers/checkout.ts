8d6c89dac2fba188280f227196d6b5fabbdd9c9f
jose1up <josealcaraz022@gmail.com>
2022-03-24 06:30:42 -0300

    resolve checkout

--- src/controllers/checkout.ts
+++ src/controllers/checkout.ts
@@ -1,26 +1,33 @@
 import { PrismaClient } from '@prisma/client'
 const {product, order} = new PrismaClient()
-import Stripe from 'stripe';
+import Stripe from "stripe";
 const {STRIPE_URL} = process.env 
-const stripe = new Stripe(STRIPE_URL|| "", {
+const stripe = new Stripe(STRIPE_URL || "", {
   apiVersion: '2020-08-27',
 });
+
 import { Request, Response } from "express";
 
 export const postCheckout = async (req:Request, res:Response)=>{
     try{
         const {userId, id}=req.body
         const findOrder = await order.findFirst({
-            where:{id:userId},include:{Order_detail:true}
+            where:{UserId:userId},include:{Order_detail:true}
     })
+
        let amount = Math.floor(Number(findOrder?.amount))
         const payment = await stripe.paymentIntents.create(
-            {amount,currency: "USD",payment_method: id,confirm: true,
+            {
+                amount,
+                currency: "USD",
+                payment_method: id,
+                confirm:true
         })
+
         if(payment.status === "succeeded"){
         const updateAllProduct = findOrder?.Order_detail.map(async(order)=>{
               await product.update({where:{
-                    id:order.id
+                    id:order.productId?order.productId:2
                 },data:{
                     stock:{
                         decrement:order.quantity
@@ -30,7 +37,7 @@ export const postCheckout = async (req:Request, res:Response)=>{
         })
         if(updateAllProduct){
             await order.update({where:{
-                id:userId
+                id:findOrder?.id
             },data:{
                 status_pay:"PAYMENT"
             }})
