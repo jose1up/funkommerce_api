41e91f43b430ad6c267553670016b9bc85b34156
jose1up <josealcaraz022@gmail.com>
2022-03-29 13:14:41 -0300

    cambio a auth0

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,7 +3,7 @@ import productRoute from "./product";
 import brandRoute from "./brand";
 import categoryRouter from "./category"
 import orderRouter from "./order";
-import userRoute from "./user";
+// import userRoute from "./user";
 import licenseRoute from "./license"
 import checkoutRoute from "./checkout"
 
@@ -13,7 +13,7 @@ router.use("/product", productRoute);
 router.use("/brand", brandRoute);
 router.use("/category", categoryRouter);
 router.use("/order", orderRouter);
-router.use("/user", userRoute);
+// router.use("/user", userRoute);
 router.use("/license", licenseRoute)
 router.use("/checkout", checkoutRoute)
 
